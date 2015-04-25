90ed5f50b51d64513c9dfa041d66d577c9d1c1a8
tgrv834 <jam834@gmail.com>
2015-04-25 02:03:21 -0600

    hello
    test

---
+++ include/aq/common.hpp
@@ -0,0 +1,13 @@
+#ifndef AQ_COMMON_HPP
+#define AQ_COMMON_HPP
+
+namespace AQ
+{
+	using Float32 = float;
+	using Float64 = double;
+
+} //namespace AQ
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <aq/common.hpp>
+
+#include <iostream>
+
+//#include "../include/aq/common.hpp"
+
+
+int main(int argc, char** argv)
+{
+	
+	
+
+	std::cout << "testing\n";
+	
+	std::cin.ignore(); //keep console on screen until input
+	return 0;
+}
