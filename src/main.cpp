67f5c70a551222be3583d8e5ecfd8eba79562cdb
tgrv834 <jam834@gmail.com>
2015-04-25 02:45:07 -0600

    a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,48 @@
 #include <aq/common.hpp>
 
+#include <GLFW\glfw3.h>
+
 #include <iostream>
+
+
 
 //#include "../include/aq/common.hpp"
 
 
 int main(int argc, char** argv)
 {
-	
-	
+	GLFWwindow* window;
+	if (!glfwInit())
+		return 1;
 
-	std::cout << "testing\n";
-	
-	std::cin.ignore(); //keep console on screen until input
+	window = glfwCreateWindow(800, 600, "AQ", nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		// render
+
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+
+
+
+	//std::cout << "testing\n";
+	//std::cin.ignore(); //keep console on screen until input
+
+	glfwTerminate();
 	return 0;
 }
