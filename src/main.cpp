df16efc2c1d3cdd8059e13e3ced2840a9e8643e2
tgrv834 <jam834@gmail.com>
2015-04-25 21:11:18 -0600

    day5 - texture, stb from image

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -43,12 +46,12 @@ int main(int argc, char** argv)
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
 
-	float vertices[] = { // x,y coords  -> x, y, r, g, b
+	float vertices[] = { // x,y coords  -> x, y, r, g, b, s, t
 		//tri strip to make square
-		0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // 2    top right
-		-0.5f, 0.5f, 1.0f, 1.0f, 1.0f, // 3   top left
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // 0   bot right
-		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f, // 1  bot left		
+		0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,  // 0    top right
+		-0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, // 1   top left
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 2   bot right
+		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // 3  bot left		
 	};
 
 
@@ -57,19 +60,7 @@ int main(int argc, char** argv)
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// GL_STATIC_DRAW  - one upload, many draw
-	// GL_DYNAMIC_DRAW - data changed, but still lots of drawing
-	// GL_STREAM_DRAW - changes a LOT, every frame
 
-	// define shaders
-	//in-line way
-//  const char* vertexShaderText = { "#version",};
-//	const char* fragmentShaderText = {
-	//from file
-	//std::string vertexShaderSource = stringFromFile("data/shaders/default.vert.glsl");
-	//const char* vertexShaderText = vertexShaderSource.c_str();
-	//std::string fragmentShaderSource = stringFromFile("data/shaders/default.frag.glsl");
-	//const char* fragmentShaderText = fragmentShaderSource.c_str();
 
 	AQ::ShaderProgram shaderProgram;
 	shaderProgram.attachShaderFromFile(AQ::ShaderType::Vertex, "data/shaders/default.vert.glsl");
@@ -77,35 +68,35 @@ int main(int argc, char** argv)
 
 	shaderProgram.bindAttributeLocation(0, "vertPosition");
 	shaderProgram.bindAttributeLocation(1, "vertColor");
+	shaderProgram.bindAttributeLocation(2, "vertTexCoord");
 	shaderProgram.link();
 	shaderProgram.use();
 
 
-	// compile shaders
-	//GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vertexShader, 1, &vertexShaderText, nullptr);
-	//glCompileShader(vertexShader);
-	//GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fragmentShader, 1, &fragmentShaderText, nullptr);
-	//glCompileShader(fragmentShader);
-
-	//GLuint shaderProgram = glCreateProgram();
-	//glAttachShader(shaderProgram, vertexShader);
-	//glAttachShader(shaderProgram, fragmentShader);
-
-	// set attribute?
-	//glBindAttribLocation(shaderProgram, 0, "vertPosition");
-	//glBindAttribLocation(shaderProgram, 1, "vertColor");
-
-	//glLinkProgram(shaderProgram);
-	//glUseProgram(shaderProgram);
+	GLuint tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 
-	//GLint uniColor = glGetUniformLocation(shaderProgram, "uniColor");
+	unsigned char* image; //gl_unsigned_byte
+	int width, height, comp;
+	image = stbi_load("data/textures/rock1.jpg", &width, &height, &comp, 0);
 	
 
+	float pixels[] = {
+		0, 0, 1,    1, 0, 0,
+		0, 1, 0,    1, 1, 0,
+	};
 
-
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glActiveTexture(GL_TEXTURE0);
+	shaderProgram.setUniform("uniTex", 0);
+	stbi_image_free(image);
+	
 
 	//handle escape / F11
 	bool running = true;
@@ -113,6 +104,14 @@ int main(int argc, char** argv)
 
 	while (running)
 	{
+
+		{
+			int width, height;
+			glfwGetWindowSize(window, &width, &height);
+			glViewport(0, 0, width, height);
+
+		}
+
 
 		// begin render
 		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
@@ -127,14 +126,17 @@ int main(int argc, char** argv)
 			//draw
 			glEnableVertexAttribArray(0); //vertposition
 			glEnableVertexAttribArray(1); //vertcolor
+			glEnableVertexAttribArray(2); //verttexcoord
 
-			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);  // from vertex data, grab 2 into attrib 0 (pos), every 5 floats
-			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const GLvoid*)(2*sizeof(float)) ); // from vertex data, grab 3 into attib 1 (color), every 5, offset by 2
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const GLvoid*)0);  // from vertex data, grab 2 into attrib 0 (pos), every 5 floats
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const GLvoid*)(2 * sizeof(float)) ); // from vertex data, grab 3 into attib 1 (color), every 5, offset by 2
+			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const GLvoid*)(5 * sizeof(float))); 
 
 			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
 			glDisableVertexAttribArray(0); //vertposition
 			glDisableVertexAttribArray(1); //vertcolor
+			glDisableVertexAttribArray(2); //verttexcoord
 		}
 
 
